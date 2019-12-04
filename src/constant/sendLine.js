b8f102f98e0b7c61c34b20dde7372ab5e9b28770
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2019-12-04 16:02:55 +0700

    Update sendLine.js

--- src/constant/sendLine.js
+++ src/constant/sendLine.js
@@ -17,7 +17,7 @@ class sendLine {
       console.log(body);
 
       request.post({
-          url: 'https://api.line.me/v2/bot/message',
+          url: 'https://api.line.me/v2/bot/message/reply',
           headers: headers,
           body: body
       }, (err, res, body) => {
